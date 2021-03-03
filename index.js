4dc3c70d2bb05e0ef302ffc7fa4abb5cfda60394
Mats <mats.romanoff@gmail.com>
2021-03-03 11:39:01 -0600

    Assigment 2

---
+++ index.js
@@ -0,0 +1,34 @@
+/*
+*
+*
+* Primari file for the API
+*
+*/
+
+
+//Dependencies
+var server = require('./lib/server');
+var workers = require('./lib/workers');
+
+
+// Declare the app
+var app = {};
+
+//Initialization funciton
+
+app.init = function(){
+
+	//Start the server
+	server.init();
+
+	//Start the workers
+	//workers.init();
+
+};
+
+
+//Execute 
+app.init();
+
+//Export the app
+module.exports = app;
